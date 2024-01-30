778367a263e9c8417b8ff8385f11d814ed155c5e
Rasika2135 <suryawanshirasika2135@gmail.com>
2024-01-30 19:03:49 +0530

    Your commit message

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -27,41 +26,9 @@ menuIcon.onclick = () => {
 }
 
 function sendEmail() {
-
     var email = 'rasikasuryawanshi24@email.com';
     var subject = 'Subject';
     var body = 'Hello,';
-
-
     var mailtoUrl = 'mailto:' + email + '?subject=' + encodeURIComponent(subject) + '&body=' + encodeURIComponent(body);
-
     window.location.href = mailtoUrl;
 }
-=======
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if(top >= offset && top < offset + height){
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*='+id +']').classList.add('active')
-            })
-        }
-    })
-}
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
-
->>>>>>> 4f7e7f7ee5fe5bed90b5ba8f0493b70db435f6e0
